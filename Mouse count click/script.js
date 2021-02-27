84933b7778dc306efc15835d38365c1027ac2272
chandan0-1 <chandanofficial.cn@gmail.com>
2021-02-27 12:14:04 +0530

    Mouser Click Counter script

---
+++ Mouse count click/script.js
@@ -0,0 +1,8 @@
+var clicked = document.getElementById("box");
+clicked.addEventListener("click",helper);
+
+var c = 0;
+function helper(){
+    c++;
+    document.getElementById("output").innerHTML = c;
+}
