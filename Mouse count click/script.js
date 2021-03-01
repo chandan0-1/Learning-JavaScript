28cb537e4d04a51860dd0047a99fc124a5980cd4
chandan0-1 <chandanofficial.cn@gmail.com>
2021-03-01 23:37:55 +0530

    Added Script file

--- Mouse count click/script.js
+++ Mouse count click/script.js
@@ -2,6 +2,7 @@ var clicked = document.getElementById("box");
 clicked.addEventListener("click",helper);
 
 var c = 0;
+
 function helper(){
     c++;
     document.getElementById("output").innerHTML = c;
